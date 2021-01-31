b1e1398a42af03686e9012edbd66449b962604e9
Alan Chang <allan850920@hotmail.com>
2021-01-31 17:54:04 +0800

    add express-session and login feature via passport

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const passport = require('passport')
+const usePassport = require('./config/passport')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -13,8 +16,17 @@ const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsSecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -36,9 +48,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
